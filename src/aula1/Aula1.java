dfd83499326787c5b63abdfeef1ba4c20efa695e
NIVIA MARIA DOMINGUES <nivia.domingues@etec.sp.gov.br>
2021-02-23 18:00:00 -0300

    commit inicial

---
+++ src/aula1/Aula1.java
@@ -0,0 +1,25 @@
+
+package aula1;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Aula1 extends JFrame {
+        Aula1()
+   {
+       setTitle("Primeira janela");
+       setSize(400,50);
+       setLocation(150,150);
+       setResizable(false);
+       getContentPane().setBackground(Color.gray);
+   }        
+ 
+    public static void main(String[] args) {
+     JFrame janela = new Aula1();
+     janela.setUndecorated(true);
+     janela.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+     janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     janela.setVisible(true);
+    }
+    
+}
